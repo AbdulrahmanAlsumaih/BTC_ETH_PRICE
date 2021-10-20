692ca87cf6aca52831415b5d2cb175939d02049d
Abdul <abdulrahman.alsumaih@icloud.com>
2021-10-20 17:13:22 -0400

    exchangeRecommended is function to recommended exchange based on cheap buy and sell price

---
+++ client/src/Api/exchangeRecommended.js
@@ -0,0 +1,14 @@
+
+function exchangeRecommended(a,b) {
+
+    if (parseInt(a) > parseInt(b))
+    {
+        return "Blockchain"
+    }
+    else
+    {
+        return "Coinbase"
+    }
+}
+
+export default exchangeRecommended;
